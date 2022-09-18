008401e341a82a0f875c23df07b9ca5f88a2cb98
siddharth raghunath <siddharthraghunath794@gmail.com>
2022-09-18 13:32:40 +0400

    Add files via upload

---
+++ cloud-run/python-bigquery/deploy-commands.sh
@@ -0,0 +1,23 @@
+# First Approach
+
+# Step-1 - Build the image 
+docker built -t demo-app-cloudrun
+
+# Step-2 - Test the image locally 
+docker run -p 8081:8081 demo-app-cloudrun
+
+# Step-3 - Tag the image locally 
+docker tag demo-app-cloudrun gcr.io/gcp-data-processing/demo-app-cloudrun
+
+# Step-4 - Push the image to Google Cloud Registry 
+docker push gcr.io/gcp-data-processing/demo-app-cloudrun
+
+
+# Second Approach 
+gcloud run deploy python-bigquery-api \
+  --source . \
+  --platform managed \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --max-instances 4 \
+  --min-instances 2 
